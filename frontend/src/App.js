9c38eaf9c722354306f0ca0c7d8c1e39537103ed
Hemalathas25 <hemalathasm04@gmail.com>
2024-03-04 11:24:51 +0530

    modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Footer from "./components/footer.jsx";
 
 const App = () => {
   return (
     <>
+    <main className='py-3'>
+      <Container>
+        { <Outlet />}
+      </Container>
+    </main>
     <Footer />
+    <ToastContainer />
     </>
-  )
-}
+  );
+};
 export default App;
